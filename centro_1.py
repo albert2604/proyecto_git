de97ee23a1cbde68cfc722a34e351452eb5bca8f
Joan Felipe Ramirez Villarraga <joanfeliperv03@gmail.com>
2024-06-01 14:46:32 +0200

    centro 1 10%

--- centro_1.py
+++ centro_1.py
@@ -2,5 +2,8 @@ class Centro:
     def __init__(self, nombre, direccion):
         self.nombre = nombre
         self.direccion = direccion
-        self.lista_canchas = []
-        
+        self.lista_canchas = []  
+
+    @staticmethod
+    def main():
+        pass 
